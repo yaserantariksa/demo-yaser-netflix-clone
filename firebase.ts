f91241e9f3fe946cab8ff6fb67cf6eea6ab85c32
yaserantariksa <yaserant@gmail.com>
2023-02-20 13:56:41 +0700

    add firebase feature

--- firebase.ts
+++ firebase.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	apiKey: "AIzaSyABvnw9qfVfB1fhgMn7TWnvVk4ciolQWmU",
+	authDomain: "netflix-clone-d8873.firebaseapp.com",
+	projectId: "netflix-clone-d8873",
+	storageBucket: "netflix-clone-d8873.appspot.com",
+	messagingSenderId: "532857727499",
+	appId: "1:532857727499:web:5867ead0be851e317f750e",
 };
 
 // Initialize Firebase
